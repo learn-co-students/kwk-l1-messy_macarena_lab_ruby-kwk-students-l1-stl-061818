f5b8f05cbdd2d1ba74fbc0de7188d3cd6ac36636
abbyw14524 <awhalen111@rsdmo.org>
2018-06-19 20:42:58 +0000

    Done.

--- macarena.rb
+++ macarena.rb
@@ -1,5 +1,13 @@
 puts "How To Do The Macarena:"
 puts ""
+puts "Right arm out in front of you, palm facing down"
+puts "Left arm out in front of you, palm facing down"
+puts "Turn your right palm up"
+puts "Turn your left palm up"
+puts "Place your right hand on your left shoulder"
+puts "Place your left hand on your right shoulder"
+puts "Bring your right hand up and around to the back of your head"
+puts "Bring your left hand up and around to the back your head"
 
 # Fix The Dance Instructions Below
 # Every line should have one instruction using puts.
